046768c0a4cac5b6f68c354b61c9c780691cac1d
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-21 01:45:13 +0200

    коментує скріпти в index.js, підключає прямо в html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './js/footer-modal.js';
-import './js/pagination.js';
+// import './js/footer-modal.js';
+// import './js/pagination.js';
 // import './js/main';
-import './js/render-by-search';
-import './js/render-by-default';
+// import './js/render-by-search';
+// import './js/render-by-default';
 // import './js/footer-modal.js';
 // import './js/back-to-top';
