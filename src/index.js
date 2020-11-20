b823e4cbd859a08ce4eba9a406e63255fde2b902
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-11-21 01:11:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './js/footer-modal.js';
 import './js/pagination.js';
-import './js/main';
+// import './js/main';
+import './js/render-by-search';
+import './js/render-by-default';
 // import './js/footer-modal.js';
 // import './js/back-to-top';
