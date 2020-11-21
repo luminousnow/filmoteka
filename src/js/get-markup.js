d5c65ff05410d06202e18c9f1b8a242551b1955d
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-21 09:04:53 +0200

    змінює назву дока, править імпорти в залежних доках

--- src/js/get-markup.js
+++ src/js/get-markup.js
@@ -1,6 +1,15 @@
+import { refs } from './get-refs.js';
+import {
+  fetchMovie,
+  createUrlForTrending,
+  createUrlForFullInfo,
+} from './create-fetch';
+import { createEventsForButtonsToWatchedToQueue } from './api-servis';
+
 import movieTpl from '../tamplates/cards.hbs';
 import modalTpl from '../tamplates/card.hbs';
 
+//
 export function renderMoviesListItem(data) {
   return movieTpl(data);
 } // додано рендер cards.hbs
@@ -47,3 +56,44 @@ export function renderModalContent(data) {
 //         </div>
 //     `;
 // }
+
+export function renderAllOnStartPage() {
+  // точка входа
+  fetchMovie(createUrlForTrending()).then(data => {
+    const films = data.results;
+
+    films.map(movieData =>
+      refs.mainWrapper.insertAdjacentHTML(
+        'beforeend',
+        renderMoviesListItem(movieData),
+      ),
+    );
+
+    refs.mainWrapper.addEventListener('click', event => {
+      refs.modal.classList.remove('hide');
+      const id = event.target.dataset.id;
+      if (id) {
+        renderFullInfo(+id);
+      }
+    });
+  });
+}
+
+function renderFullInfo(id) {
+  fetchMovie(createUrlForFullInfo(id))
+    .then(data => {
+      refs.modal.innerHTML = renderModalContent(data); // тут передаю полученую дату в модалку полной инфи о фильме
+      const close = document.querySelector('.js-close');
+      close.addEventListener('click', () => {
+        refs.modal.classList.add('hide');
+      });
+      data = null;
+      return id;
+    })
+    .then(id => {
+      createEventsForButtonsToWatchedToQueue(id); // для тех кнопок что в модалке с полной инфой о фильме чтоб id фильма записать в localstorage
+    })
+    .catch(e => {
+      console.log(e);
+    });
+}
