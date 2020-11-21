b1f7d72fa42bfec23c65a9c9a00433c153ef776c
xdpiqbx <xdpiqbx@gmail.com>
2020-11-21 21:07:13 +0200

    Добавляет разметку пагинации и fixes в js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@
 // import './js/main';
 // import './js/render-by-search';
 // import './js/render-by-default';
-import './js/back-to-top';
-import './js/footer-modal.js';
+// import './js/footer-modal.js';
+// import './js/back-to-top';
