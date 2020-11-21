53d8504a032a0bfebd3361c91de0a7e90acb0fca
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-21 17:44:34 +0200

    повертає чотиризначне число року, чистить сміття

--- src/js/get-markup.js
+++ src/js/get-markup.js
@@ -16,50 +16,15 @@ import modalTpl from '../tamplates/card.hbs';
 
 // рендер колекції карток фільмів
 export function renderMoviesListItem(data) {
+  data.release_date = data.release_date.slice(0, 4); // повертає формат дати в рік з чотирьох чисел"2000"
   return movieTpl(data);
 }
-// export function renderMoviesListItem(data){
-//     return `
-//         <li class="js-films-wrapper-list__item" data-id="${data.id}">
-//             <img width="100px" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" data-id="${data.id}"/>
-//             <p data-id="${data.id}">${data.title || data.name}</p>
-//         </li>
-//     `
-// }
 
 // рендер однієї картки фільму для модалки
 export function renderModalContent(data) {
   return modalTpl(data);
 }
-// export function renderModalContent(data) {
-//   // тут описана тестовая модалка, все данные приходят норм
 
-//   function renderGenres(data) {
-//     return `<span>${data.genres.map(genre => genre.name).join(', ')}</span>`;
-//   }
-
-//   return `
-//         <div>
-//             <img width='300' src="https://image.tmdb.org/t/p/w500/${
-//               data.poster_path
-//             }"/>
-//         </div>
-//         <div>
-//             <h3>${data.name || data.title}</h3>
-//             <p>vote / votes ----- ${data.vote_average} / ${data.vote_count}</p>
-//             <p>Popularity ${data.popularity}</p>
-//             <p>Original title: ${data.original_title || data.original_name}</p>
-//             <p>Genre: ${renderGenres(data)}</p>
-//             <h4>About</h4>
-//             <p>${data.overview}</p>
-//             <div class="js-btns-add">
-//                 <button class="to-watched">Add to watched</button>
-//                 <button class="to-queue">Add to queue</button>
-//                 <button class='js-close'>Close</button>
-//             </div>
-//         </div>
-//     `;
-// }
 const loader = document.querySelector('.loader-ellips');
 
 export function renderAllOnStartPage() {
