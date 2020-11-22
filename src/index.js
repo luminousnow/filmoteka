daef4c88fdd50cd839430e2a6dfaa52b36c3e29b
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-22 12:06:46 +0200

    чистить закоментоване сміття

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-// import './js/footer-modal.js';
 // import './js/pagination.js';
-// import './js/main';
 // import './js/render-by-search';
-// import './js/render-by-default';
 import './js/back-to-top';
 import './js/footer-modal.js';
