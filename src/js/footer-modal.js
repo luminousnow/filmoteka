f1eca74723ac16db1ad4809db8790dabb14f6d7b
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-22 09:38:57 +0200

    доповнює список, сортує в алфавітному порядку

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -10,12 +10,13 @@ function onFooterBtnClick(event) {
   const instance = basicLightbox.create(`
     <h3 class="team-title">Над проектом працювали :</h3>
     <ul class="team">
-      <li class="team-list">Denys Kravchuk</li>
-      <li class="team-list">Serhiy Stepanov</li>
       <li class="team-list">Andrey Kovsh</li>
-      <li class="team-list">Valentyna Chudik</li>
-      <li class="team-list">Marina Vetokh</li>
       <li class="team-list">Andrii Lavrenchuk</li>
+      <li class="team-list">Artem Kiev</li>
+      <li class="team-list">Denys Kravchuk</li>
+      <li class="team-list">Marina Vetokh</li>
+      <li class="team-list">Serhiy Stepanov</li>
+      <li class="team-list">Valentyna Chudik</li>
     </ul>
 `);
   instance.show();
