5d84fcbe65ab492a6eabd2a8556d2c6cbf17794e
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-22 10:00:54 +0200

    Updates

--- main-home.285249f8.js
+++ main-home.285249f8.js
@@ -2825,7 +2825,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64711" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57819" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
